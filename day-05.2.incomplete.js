bdc15c1209c461ee95d5e4d6ed5f1bfde6663cee
wersimmon <185822+wersimmon@users.noreply.github.com>
2019-11-25 12:51:48 -0800

    Update 5.2 code based on new understanding of requirements

--- day-05.2.incomplete.js
+++ day-05.2.incomplete.js
@@ -7,59 +7,33 @@ let typedPolymerLengths = {};
 types.forEach(function(type) {
 	let typedPolymer = polymer;
 	
-	(function() {
-		process.stdout.write(`For typedPolymer ${type}, ${typedPolymer.length} units `);
-		let spliced = false;
-		while(true) {
-			spliced = false;
-			for(let i = 0; i < typedPolymer.length; i++) {
-				let thisUnit = typedPolymer[i];
-				let nextUnit = typedPolymer[i + 1];
-				if(
-					thisUnit == type.toUpperCase() && nextUnit == type.toLowerCase() ||
-					thisUnit == type.toLowerCase() && nextUnit == type.toUpperCase()
-				) {
-					typedPolymer = typedPolymer.substring(0, i) + typedPolymer.substring(i + 2);
-					spliced = true;
-					break;
-				}
-			}
-			
-			if(spliced == true) {
-				continue;
-			} else {
-				break;
-			}
-		}
-	})();
-	process.stdout.write(`pre-reacted to ${typedPolymer.length};\n`);
+	process.stdout.write(`TypedPolymer ${type}`);
+	typedPolymer = typedPolymer.replace(new RegExp(type, "gi"), "");
+	process.stdout.write(` pre-reacted to ${typedPolymer.length}`);
 	
-	(function() {
-		process.stdout.write(`  and ${typedPolymer.length} `)
-		let spliced = false;
-		while(true) {
-			spliced = false;
-			for(let i = 0; i < typedPolymer.length; i++) {
-				let thisUnit = typedPolymer[i];
-				let nextUnit = typedPolymer[i + 1];
-				if(
-					thisUnit == thisUnit.toUpperCase() && nextUnit == thisUnit.toLowerCase() ||
-					thisUnit == thisUnit.toLowerCase() && nextUnit == thisUnit.toUpperCase()
-				) {
-					typedPolymer = typedPolymer.substring(0, i) + typedPolymer.substring(i + 2);
-					spliced = true;
-					break;
-				}
-			}
-			
-			if(spliced == true) {
-				continue;
-			} else {
+	let spliced = false;
+	while(true) {
+		spliced = false;
+		for(let i = 0; i < typedPolymer.length; i++) {
+			let thisUnit = typedPolymer[i];
+			let nextUnit = typedPolymer[i + 1];
+			if(
+				thisUnit == thisUnit.toUpperCase() && nextUnit == thisUnit.toLowerCase() ||
+				thisUnit == thisUnit.toLowerCase() && nextUnit == thisUnit.toUpperCase()
+			) {
+				typedPolymer = typedPolymer.substring(0, i) + typedPolymer.substring(i + 2);
+				spliced = true;
 				break;
 			}
 		}
-	})();
-	process.stdout.write(`reacted to ${typedPolymer.length} units long.\n`);
+		
+		if(spliced == true) {
+			continue;
+		} else {
+			break;
+		}
+	}
+	process.stdout.write(` and reacted to ${typedPolymer.length}.\n`);
 	
 	typedPolymerLengths[type] = typedPolymer.length - 1;
 });
